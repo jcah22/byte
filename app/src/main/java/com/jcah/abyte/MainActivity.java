73ec40f234f84eb5a76800365b81f5844f35370d
julio22 <jcah549@gmail.com>
2020-09-20 17:37:03 -0500

    creando el menu e implementando lottie

--- app/src/main/java/com/jcah/abyte/MainActivity.java
+++ app/src/main/java/com/jcah/abyte/MainActivity.java
@@ -2,13 +2,36 @@ package com.jcah.abyte;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSplash();
+
+
+    }
+
+    private void getSplash() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },DURACION_SPLASH);
+
+
     }
 }

81d7c61b1490944819767d21716e2c64b92d597b
julio22 <jcah549@gmail.com>
2020-09-20 19:55:58 -0500

    agregando lodo de villalpando

--- app/src/main/java/com/jcah/abyte/EnamoroActivity.java
+++ app/src/main/java/com/jcah/abyte/EnamoroActivity.java
@@ -3,9 +3,14 @@ package com.jcah.abyte;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class EnamoroActivity extends AppCompatActivity {
+
+    ImageView diezk,catorcek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,25 @@ public class EnamoroActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        diezk = findViewById(R.id.img10k);
+        catorcek = findViewById(R.id.img14k);
+
+
+        diezk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentDiez = new Intent(EnamoroActivity.this,ArgollasDiezActivity.class);
+                startActivity(intentDiez);
+            }
+        });
+
+        catorcek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCatorce = new Intent(EnamoroActivity.this,ArgollasCatorceActivity.class);
+                startActivity(intentCatorce);
+            }
+        });
     }
 }

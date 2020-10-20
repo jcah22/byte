869b484fcd1824b3549b6e61000059d6d41aabb4
julio <jcah549@gmail.com>
2020-10-20 14:05:28 -0500

    instanciando cards en mosha activity

--- app/src/main/java/com/jcah/abyte/MoshaActivity.java
+++ app/src/main/java/com/jcah/abyte/MoshaActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.skydoves.elasticviews.ElasticCardView;
 
 public class MoshaActivity extends AppCompatActivity {
+
+    ElasticCardView cardPlataCollares;
+    ElasticCardView cardPlataCadenas;
+    ElasticCardView cardPlataArracadasAretes;
+    ElasticCardView cardPlataDijes;
+    ElasticCardView cardPlataEsclavas;
+    ElasticCardView cardPlataAnillos;
+    ElasticCardView cardPlataPulseras;
+    ElasticCardView cardPlataReligiosos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,74 @@ public class MoshaActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+
+        cardPlataCollares = findViewById(R.id.idCardPlataCollares);
+        cardPlataCadenas = findViewById(R.id.idCardPlataCadenas);
+        cardPlataArracadasAretes = findViewById(R.id.idCardPlataArracadasAretes);
+        cardPlataDijes = findViewById(R.id.idCardPlataDijes);
+        cardPlataEsclavas = findViewById(R.id.idCardPlataEsclavas);
+        cardPlataAnillos = findViewById(R.id.idCardPlataAnillos);
+        cardPlataPulseras = findViewById(R.id.idCardPlataPulseras);
+        cardPlataReligiosos = findViewById(R.id.idCardPlataReligioso);
+
+
+        cardPlataCollares.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataCadenas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataArracadasAretes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataDijes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataEsclavas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataAnillos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataPulseras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardPlataReligiosos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }

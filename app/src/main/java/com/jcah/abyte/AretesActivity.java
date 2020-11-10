e31184449c5a9029f49e6e9ecb2a6718e57d7eaf
julio <jcah549@gmail.com>
2020-11-10 16:58:06 -0600

    implementando aretes activity

--- app/src/main/java/com/jcah/abyte/AretesActivity.java
+++ app/src/main/java/com/jcah/abyte/AretesActivity.java
@@ -1,14 +1,68 @@
 package com.jcah.abyte;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.jcah.abyte.adapters.ReciclerAreteAdapter;
+import com.jcah.abyte.models.Aretes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AretesActivity extends AppCompatActivity {
+
+     private RecyclerView rvLista;
+        private ReciclerAreteAdapter adapter;
+        private List<Aretes> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aretes);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+        initViews();
+        initValues();
+
     }
+
+    private void initViews(){
+        rvLista = findViewById(R.id.rvAretes);
+
+    }
+
+    private void initValues() {
+
+        LinearLayoutManager manager = new  LinearLayoutManager(this);
+        rvLista.setLayoutManager(manager);
+        items = getItems();
+        adapter = new ReciclerAreteAdapter(items);
+        rvLista.setAdapter(adapter);
+
+    }
+
+    private List<Aretes> getItems(){
+        List<Aretes>  itemList = new ArrayList<>();
+
+        itemList.add(new Aretes(R.drawable.gra011at_1,"GRA011AT","Aretes angel niña corazon","0.7 gr"));
+        itemList.add(new Aretes(R.drawable.gra020at_1,"GRA020AT","Aretes Corazón","0.7 gr"));
+        itemList.add(new Aretes(R.drawable.gra043hg_1,"GRA043HG","Arete Huggies flores con piedra","2.8 gr"));
+        itemList.add(new Aretes(R.drawable.gra044hg_2,"GRA044HG","Arete Huggies corazones calados con piedra","2.6 gr"));
+        itemList.add(new Aretes(R.drawable.gra180hg_1,"GRA180HG","Arete Huggies corazón con hueco y piedras","3.0 gr;"));
+        itemList.add(new Aretes(R.drawable.gra181hg_1,"GRA181HG","Arete Huggies corazón piedras rosas","2.9 gr"));
+        itemList.add(new Aretes(R.drawable.gra182hg,"GRA182HG","Arete Huggies tipo herradura con piedras azules","2.9 gr"));
+
+        return  itemList;
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/jcah/abyte/adapters/ReciclerAreteAdapter.java
@@ -0,0 +1,75 @@
+package com.jcah.abyte.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jcah.abyte.R;
+import com.jcah.abyte.models.Aretes;
+
+import java.util.List;
+
+public class ReciclerAreteAdapter extends RecyclerView.Adapter<ReciclerAreteAdapter.RecyclerHolder> {
+
+    private List<Aretes> items;
+
+    public ReciclerAreteAdapter(List<Aretes> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_aretes,parent,false);
+        return new RecyclerHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final RecyclerHolder holder, int position) {
+
+        final  Aretes item = items.get(position);
+        holder.foto.setImageResource(item.getFoto());
+        holder.codigo.setText(item.getCodigo());
+        holder.descripcion.setText(item.getDescripcion());
+        holder.pesoProm.setText(item.getPesoProm());
+
+        // TODO - IMPLEMENTAR CLICK EN CADA ITEM
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(holder.itemView.getContext(), "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+    public class RecyclerHolder extends RecyclerView.ViewHolder {
+
+        private ImageView foto;
+        private TextView codigo;
+        private TextView descripcion;
+        private TextView pesoProm;
+
+
+        public RecyclerHolder(@NonNull View itemView) {
+            super(itemView);
+            foto = itemView.findViewById(R.id.imgAretes);
+            codigo = itemView.findViewById(R.id.txtCodigoAretes);
+            descripcion = itemView.findViewById(R.id.txtDescripcionAretes);
+            pesoProm = itemView.findViewById(R.id.txtPesoPromAretes);
+        }
+    }
+}
